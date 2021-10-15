a23c2f0c3efe9ff86d7d91f666b1513264384c95
Clayers <Claydralthiace@gmail.com>
2021-10-15 20:15:43 +0300

    file

---
+++ 2_Quest/FillFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _2_Quest
+{
+    internal class FillFile
+    {
+        public void Fill(string NameFile, List<Word> Words)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + NameFile + ".txt";
+
+            FileStream file = new FileStream(path, FileMode.OpenOrCreate);
+
+            StreamWriter fnew = new StreamWriter(file);
+            for (int i = 0; i < Words.Count; i++)
+            {
+                fnew.WriteLine(String.Format(Words[i].Words.ToString() + "(" + Words[i].Quantity.ToString() + ")"));
+            }
+            fnew.Close();
+        }
+    }
+}
