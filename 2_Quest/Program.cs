1e497ddbb3b22f688fd84fe81975cf74aa8828a6
Clayers <Claydralthiace@gmail.com>
2021-10-19 15:21:51 +0300

    New Lib SearchWord

--- 2_Quest/Program.cs
+++ 2_Quest/Program.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using MyLIbSearchWord;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using System.Reflection;
 
 namespace _2_Quest
 {
@@ -17,13 +23,17 @@ namespace _2_Quest
             WordList.Add("plus");
 
             return WordList;
+
+
         }
 
         private static void Main(string[] args)
         {
+            
+
+
             List<string> PathFile = new List<string>();// Лист с путями файлов
             SearchFile SearchFile = new SearchFile();
-            SearchWord SearchWord = new SearchWord();
             FillFile FillFile = new FillFile();
             List<Word> Words = new List<Word>();//найденные слова
             List<string> WordList = new List<string>();//искомые слова
@@ -34,15 +44,31 @@ namespace _2_Quest
             {
                 Data = Data + SearchFile.GetStringFile(PathFile[i]);
             }
+            
             if (Switch == false)
             {
-                FillFile.Fill("New", SearchWord.SearchAllWord(Data));
+                FillFile.Fill("New", SortingRec((Dictionary<string, int>)typeof(SearchWord).GetMethod("SearchAllWord", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(new SearchWord(), new object[] { Data })));
             }
             if (Switch == true)
             {
                 FillSearchWord(WordList);
-                FillFile.Fill("New", SearchWord.SearchWords(Data, WordList));
+                FillFile.Fill("New", SortingRec((Dictionary<string, int>)typeof(SearchWord).GetMethod("SearchWords", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(new SearchWord(), new object[] { Data })));
             }
+        }
+
+
+        static public List<Word> SortingRec(Dictionary<string, int> Words)
+        {
+            List<Word> WordsList = new List<Word>();
+
+            foreach (var item in Words)
+            {
+                WordsList.Add(new Word(item.Value, item.Key));
+            }
+
+            WordsList = WordsList.OrderByDescending(x => x.Quantity).ToList();
+
+            return WordsList;
         }
     }
 }
